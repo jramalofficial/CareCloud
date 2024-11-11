da141e9dda5ff29f6f40350ba61450b8f43830d2
AMAL J R <164713916+jramalofficial@users.noreply.github.com>
2024-11-11 19:56:12 +0530

    Delete CareCloud directory

--- CareCloud/src/main/java/com/example/demo/repository/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Patient;
-
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-
-}
